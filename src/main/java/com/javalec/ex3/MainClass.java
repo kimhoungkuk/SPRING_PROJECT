5887169775cb21c9f9d2328ea90acbb77bc3db7b
DESKTOP-NVS2M80\kimhk0 <kimhk1571@gmail.com>
2017-11-25 14:36:02 +0900

    스프링 프로젝트

--- src/main/java/com/javalec/ex3/MainClass.java
+++ src/main/java/com/javalec/ex3/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
 		String configLocation = "classpath:applicationCTX2.xml";
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation); // 스프링 컨테이너 생성
 		Myinfo myinfo = ctx.getBean("myInfo",Myinfo.class); // 스프링 컨테이너에서 컴포넌트 가져옴
-		myinfo.getInfo();
+		myinfo.getInfo(); 
 		ctx.close();     
 		
 	}
