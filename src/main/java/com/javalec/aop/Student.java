73653e128ff76b5f30e60b06808cfbf02fdf9ea8
DESKTOP-NVS2M80\kimhk0 <kimhk1571@gmail.com>
2018-01-10 21:16:44 +0900

    AOP 스터디

---
+++ src/main/java/com/javalec/aop/Student.java
@@ -0,0 +1,41 @@
+package com.javalec.aop;
+
+public class Student {
+	
+	private String name;
+	private String age;
+	private int gradeNum;
+	private int classNum;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public int getGradeNum() {
+		return gradeNum;
+	}
+	public void setGradeNum(int gradeNum) {
+		this.gradeNum = gradeNum;
+	}
+	public int getClassNum() {
+		return classNum;
+	}
+	public void setClassNum(int classNum) {
+		this.classNum = classNum;
+	}
+	
+	public void getStudentInfo(){
+		System.out.println("이름 : " + getName());
+		System.out.println("나이 : " + getAge());
+		System.out.println("학년 : " + getGradeNum());
+		System.out.println("반 : " + getClassNum());
+	}
+}
